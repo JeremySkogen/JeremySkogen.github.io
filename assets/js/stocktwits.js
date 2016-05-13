5466121d95e2a37acd2b278b12c393f370ecd583
jskogen <jeremyskogen@gmail.com>
2016-05-13 09:13:24 -0700

    Updated horse races

---
+++ assets/js/stocktwits.js
@@ -0,0 +1,156 @@
+var StockTwits = {
+  getTrending: function(){
+    return [
+      {
+        "id": 9721,
+        "symbol": "CLVS",
+        "title": "Clovis Oncology, Inc. "
+      },
+      {
+        "id": 12844,
+        "symbol": "AVXL",
+        "title": "Anavex Life Sciences Corp."
+      },
+      {
+        "id": 7529,
+        "symbol": "UA",
+        "title": "Under Armour, Inc."
+      },
+      {
+        "id": 5222,
+        "symbol": "ETE",
+        "title": "Energy Transfer Equity, L.P."
+      },
+      {
+        "id": 12709,
+        "symbol": "CHMA",
+        "title": "Chiasma"
+      },
+      {
+        "id": 11354,
+        "symbol": "ANFI",
+        "title": "Amira Nature Foods Ltd."
+      },
+      {
+        "id": 9636,
+        "symbol": "LNKD",
+        "title": "LinkedIn"
+      },
+      {
+        "id": 3299,
+        "symbol": "QLGC",
+        "title": "QLogic Corp."
+      },
+      {
+        "id": 11188,
+        "symbol": "UVXY",
+        "title": "ProShares Trust Ultra VIX Short-Term Futures ETF"
+      },
+      {
+        "id": 1582,
+        "symbol": "DEPO",
+        "title": "DepoMed Inc."
+      },
+      {
+        "id": 4202,
+        "symbol": "AA",
+        "title": "Alcoa Inc."
+      },
+      {
+        "id": 679,
+        "symbol": "SPX",
+        "title": "S&P 500 Index"
+      },
+      {
+        "id": 2022,
+        "symbol": "GLRE",
+        "title": "Greenlight Capital Re, Ltd."
+      },
+      {
+        "id": 11085,
+        "symbol": "KORS",
+        "title": "Michael Kors Holdings Ltd"
+      },
+      {
+        "id": 5576,
+        "symbol": "GPS",
+        "title": "Gap Inc."
+      },
+      {
+        "id": 1122,
+        "symbol": "BOOT",
+        "title": "Boot Barn Holdings Inc."
+      },
+      {
+        "id": 11319,
+        "symbol": "WMC",
+        "title": "Western Asset Mortgage Capital Corporation"
+      },
+      {
+        "id": 7871,
+        "symbol": "FB",
+        "title": "Facebook"
+      },
+      {
+        "id": 7414,
+        "symbol": "TK",
+        "title": "Teekay Corporation"
+      },
+      {
+        "id": 11174,
+        "symbol": "GOGO",
+        "title": "GoGo Inc."
+      },
+      {
+        "id": 11711,
+        "symbol": "AAL",
+        "title": "American Airlines Group"
+      },
+      {
+        "id": 7069,
+        "symbol": "RRC",
+        "title": "Range Resources Corporation"
+      },
+      {
+        "id": 9440,
+        "symbol": "BONA",
+        "title": "Bona Film Group Limited"
+      },
+      {
+        "id": 11810,
+        "symbol": "DDC",
+        "title": "Dominion Diamond Corporation"
+      },
+      {
+        "id": 7826,
+        "symbol": "XOP",
+        "title": "SPDR S&P Oil & Gas Exploration & Prod"
+      },
+      {
+        "id": 1260,
+        "symbol": "CELG",
+        "title": "Celgene Corporation"
+      },
+      {
+        "id": 11572,
+        "symbol": "XLRN",
+        "title": "Acceleron Pharma, Inc."
+      },
+      {
+        "id": 9717,
+        "symbol": "DUST",
+        "title": "Direxion Daily Gold Miners Bear 3X Shrs"
+      },
+      {
+        "id": 513,
+        "symbol": "SA",
+        "title": "Seabridge Gold, Inc."
+      },
+      {
+        "id": 2756,
+        "symbol": "MTSC",
+        "title": "MTS Systems Corp."
+      }
+    ]
+  }
+}
